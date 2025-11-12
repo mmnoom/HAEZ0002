438f04a6f12e26ef803e1bf8bfff81b7fd726e37
mmnoom <sueric0809@naver.com>
2025-11-12 21:40:14 +0900

    week 9

---
+++ thisisjava/src/ch06/sec06/exam01/CarExample.java
@@ -0,0 +1,17 @@
+package ch06.sec06.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car mycar = new Car();
+		
+		//Car 객체의 필드값 읽기
+		System.out.println("모델명: " + mycar.model);
+		System.out.println("모델명: " + mycar.start);
+		System.out.println("모델명: " + mycar.speed);
+		
+		
+	}
+
+}
---
+++ thisisjava/src/ch06/sec08/exam01/CalculatorExample.java
@@ -0,0 +1,31 @@
+package ch06.sec08.exam01;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Calculator 객체 생성
+		Calculator myCalc = new Calculator();
+		
+		// 리천값이 없는 powerOn()메소드 호출
+		myCalc.powerOn();
+		
+		//plus() 메소드 호출시 5와 6을 매개값으로 제공하고
+		// 덧셈 결과를 리턴받아 result1 변수에 대입
+		int result1 = myCalc.plus(5, 6);
+		System.out.println("result1 : " + result1);
+		
+		int x = 10;
+		int y = 4;
+		//divide() 메소드 호출 시 변수 x, y의 값을 매개값으로 제공하고 
+		// 나눗셈의 결과를 리턴받아 result2 변수에 대입
+		double result2 = myCalc.divide(x, y);
+		System.out.println("result2 : " +  result2);
+		
+		// 리천값이 없는 powerOn()메소드 호출
+		myCalc.powerOff();
+		
+
+	}
+
+}
